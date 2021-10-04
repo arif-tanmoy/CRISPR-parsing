9af8c13832f7c00f34d9e8f793c96eea4b3479f7
arif tanmoy <arif.tanmoy@gmail.com>
2021-10-04 09:33:47 +0600

    chnaged author email

--- Summarize_CRISPRDetect.py
+++ Summarize_CRISPRDetect.py
@@ -3,7 +3,7 @@
 #Citing CRISPRDetect: Biswas A et al (2016). https://bmcgenomics.biomedcentral.com/articles/10.1186/s12864-016-2627-0
 
 #Author: Arif Tanmoy
-#Email: arif.m.tanmoy@gmail.com
+#Email: arif.tanmoy@gmail.com
 #USE: Python SummarizeCRISPRDetect.py <CRISPRDetect_result_file> <Parsed_File>
 #OUTPUT_FORMAT: primary_Repeat, alternative_repeat, repeat_no, repeat_len, identity, spacer_length, contig/seq_name, contig/seq_end_position, questionable_array(yes/no), score, LeftFlankSeq, RightFlankSeq
 #**Silent line 60 if you do not want to print/see the results on commandline**
